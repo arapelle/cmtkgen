484fae5b681134c0c3d479e7ac44d272fa4d443a
Aymeric Pellé <aympelle@gmail.com>
2020-09-23 11:45:50 +0200

    v0.3.0:CMGN#5: Possibility to create a custom input CMake package confile file..

--- create_cpp_honly_lib_project.py
+++ create_cpp_honly_lib_project.py
@@ -32,11 +32,14 @@ add_cpp_library_examples(SHARED ${{PROJECT_NAME}}\n\
 # library_project_cmakelists_contents():
 def library_project_cmakelists_contents(project_cmakelists_path:str, project_name:str, project_version:str, \
                                         cmake_major:str, cmake_minor:str, build_in_tree_allowed:bool, \
-                                        create_version_header:bool, cmake_project_config_type:str,
+                                        create_version_header:bool, cmake_project_config_type:str, cmake_project_config_file:str, \
                                         cpp_version:str):
     contents = project_cmakelists_contents("CppLibraryProject", project_cmakelists_path, project_name, project_version, \
                                            cmake_major, cmake_minor, build_in_tree_allowed)
     create_version_header_code = "    OUTPUT_VERSION_HEADER \"version.hpp\"\n" if create_version_header else ""
+    package_config_code = cmake_project_config_type + "_PACKAGE_CONFIG_FILE"
+    if cmake_project_config_type:
+        package_config_code += " " + cmake_project_config_file
     return contents + "\
 include(CTest)\n\
 \n\
@@ -52,7 +55,7 @@ add_cpp_honly_library(${{PROJECT_NAME}}\n\
 \n\
 # Link targets:\n\
 # find_package(TBB 2018 REQUIRED CONFIG)\n\
-# cpp_library_targets_link_libraries(${PROJECT_NAME} HEADER_ONLY PUBLIC TBB::tbb)\n\
+# cpp_library_targets_link_libraries(${{PROJECT_NAME}} HEADER_ONLY PUBLIC TBB::tbb)\n\
 \n\
 # Install C++ library\n\
 install_cpp_library_targets(${{PROJECT_NAME}}\n\
@@ -62,7 +65,7 @@ install_cpp_library_targets(${{PROJECT_NAME}}\n\
 \n\
 # Install package\n\
 install_package(${{PROJECT_NAME}}\n\
-                {cmake_project_config_type}_PACKAGE_CONFIG_FILE\n\
+                {package_config_code}\n\
                 EXPORT_NAMES ${{PROJECT_NAME}})\n\
 \n\
 if(${{PROJECT_NAME}}_BUILD_EXAMPLES)\n\
@@ -73,18 +76,18 @@ if(${{PROJECT_NAME}}_BUILD_TESTS AND BUILD_TESTING)\n\
     add_subdirectory(test)\n\
 endif()\n\
 \n\
-#-----\n".format(create_version_header_code=create_version_header_code, cmake_project_config_type=cmake_project_config_type, \
+#-----\n".format(create_version_header_code=create_version_header_code, package_config_code=package_config_code, \
                  cpp_version=cpp_version, pname=project_name)
 
 # create_project_cmakelists()
 def create_project_cmakelists(project_cmakelists_path:str, project_name:str, project_version:str, \
                               cmake_major:str, cmake_minor:str, build_in_tree_allowed:bool, \
-                              create_version_header:bool, cmake_project_config_type:str,
+                              create_version_header:bool, cmake_project_config_type:str, cmake_project_config_file:str, \
                               cpp_version:str):
     with open(project_cmakelists_path, "w") as project_cmakelists_file:
         content = library_project_cmakelists_contents(project_cmakelists_path, project_name, project_version, \
                                                       cmake_major, cmake_minor, build_in_tree_allowed, create_version_header, \
-                                                      cmake_project_config_type, cpp_version)
+                                                      cmake_project_config_type, cmake_project_config_file, cpp_version)
         project_cmakelists_file.write(content)
 
 #--------------------------------------------------------------------------------
@@ -101,8 +104,12 @@ class Cmtk_honly_library_project_creator(Cmtk_shared_project_creator):
         # CMakeLists.txt
         self._cml_create_version_header = init_bool_parameter("Do you want a version header file?")
         default_cmake_project_config_type = "VERBOSE"
-        self._cml_cmake_project_config_type = init_parameter("Project config type (BASIC | VERBOSE)", default_cmake_project_config_type, \
-                                                             lambda type: type in ["BASIC", "VERBOSE"])
+        self._cml_cmake_project_config_type = init_parameter("Project config type (BASIC | VERBOSE | INPUT)", default_cmake_project_config_type, \
+                                                             lambda type: type in ["BASIC", "VERBOSE", "INPUT"])
+        if self._cml_cmake_project_config_type == "INPUT":
+            default_cmake_package_config_file = "CMake-package-config.cmake.in"
+            self._cmake_project_config_file = init_parameter("Input package config file name?", default_cmake_package_config_file, \
+                                                                lambda filename: len(filename) > 0)
         pass
 
     def _create_dir_tree(self):
@@ -119,7 +126,12 @@ class Cmtk_honly_library_project_creator(Cmtk_shared_project_creator):
         create_project_cmakelists(project_cmakelists_path, self._project_name, self._project_version, \
                                   self._cmake.metadata().major_version(), self._cmake.metadata().minor_version(), \
                                   self._cml_build_in_tree_allowed, self._cml_create_version_header, \
-                                  self._cml_cmake_project_config_type, self._cml_cpp_version)
+                                  self._cml_cmake_project_config_type, self._cmake_project_config_file, \
+                                  self._cml_cpp_version)
+        # Write project input CMake package config, if needed:
+        if self._cml_cmake_project_config_type == "INPUT":
+            input_cmake_package_config_path = "{proot}/{filename}".format(proot=self._project_name, filename=self._cmake_project_config_file)
+            self.__create_input_cmake_package_config_file(input_cmake_package_config_path)
         # Write project/test/CMakeLists.txt
         test_source_path = "{proot}/{test}/{proot}_tests.cpp".format(proot=self._project_name, test="test")
         self.__create_test_file(test_source_path)
@@ -136,6 +148,22 @@ class Cmtk_honly_library_project_creator(Cmtk_shared_project_creator):
         cmake_example_cmakelists_path = "{proot}/{example}/CMakeLists.txt".format(proot=self._project_name, example="example/basic_cmake_project")
         self.__create_cmake_example_cmakelists(cmake_example_cmakelists_path)
         super()._create_files()
+        pass
+
+    def __create_input_cmake_package_config_file(self, input_cmake_package_config_path:str):
+        with open(input_cmake_package_config_path, "w") as input_cmake_package_config_file:
+            content = """
+@PACKAGE_INIT@
+
+# include(CMakeFindDependencyMacro)
+# find_dependency(TBB 2018 CONFIG)
+
+include(${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@.cmake)
+check_required_components(@PROJECT_NAME@)
+
+message(STATUS "Found package @PROJECT_NAME@ @PROJECT_VERSION@")
+"""
+            input_cmake_package_config_file.write(content)
         pass
 
     def __create_test_file(self, test_source_path:str):
